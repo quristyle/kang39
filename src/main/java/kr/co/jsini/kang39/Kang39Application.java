6afe01ecc126fd52bbcdee717a232dba97b834f5
quristyle <quristyle@gmail.com>
2023-12-05 12:24:06 +0900

    maven tomcat9 용 세팅

---
+++ src/main/java/kr/co/jsini/kang39/Kang39Application.java
@@ -0,0 +1,16 @@
+package kr.co.jsini.kang39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Kang39Application  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kang39Application.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/kr/co/jsini/kang39/Kang39ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jsini.kang39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kang39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
